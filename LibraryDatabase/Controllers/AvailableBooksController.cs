09877254f1664d4df0145a676311a3ed7e8203af
Rigney <krigney@proforma.com>
2018-04-22 19:45:54 -0400

    Added 'Search for available(checked in) books.

---
+++ LibraryDatabase/Controllers/AvailableBooksController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LibraryDatabase.Models;
+using LibraryDatabase.Data;
+using LibraryDatabase.ViewModels;
+
+namespace LibraryDatabase.Controllers
+{
+    public class AvailableBooksController : ApiController
+    {
+        [HttpGet]
+
+        public IEnumerable<Book> SearchAvailableBooks()
+        {
+            var db = new DataContext();
+            var query = db.Books.Include(i => i.Author).Include(i => i.Genre);
+            var results = query.Where(book => book.IsCheckedOut == false);
+            return results.ToList();
+        }
+    }
+}
+
