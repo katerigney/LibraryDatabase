2d3d90d7d312faf4df6f3def41b20d1a48c9a1a4
Rigney <krigney@proforma.com>
2018-04-23 15:59:29 -0400

    Get all books displaying and basic UI plan.

--- LibraryDatabase/Scripts/app.js
+++ LibraryDatabase/Scripts/app.js
@@ -1,12 +1,25 @@
-﻿//angular.
-//    module("helloWorldApp", [])
-//    .controller("mainController", ["$scope", "$http", ($scope, $http) => {
-//        $scope.currentTime = new Date();
-//        $http({
-//            method: "GET",
-//            url: "/api/book"
-//        }).then(resp => {
-//            console.log(resp.data);
-//            $scope.books = resp.data;
-//        })
-//    }]);
+﻿angular.
+    module("libraryApp", [])
+    .controller("mainController", ["$scope", "$http", ($scope, $http) => {
+
+        $scope.currentTime = new Date();
+
+        $http({
+            method: "GET",
+            url: "/api/books"
+        }).then(resp => {
+            console.log(resp.data);
+            $scope.books = resp.data;
+        })
+    }]);
+
+//PUT WILL LOOK LIKE:
+//$http({
+//method: "PUT",//what I am doing
+  //  url: "/api/book"//where am I doing it
+    //data: JSON.stringify(email: "email from UI text box goes here.")
+
+
+      //  }).then(resp => {
+    //console.log(resp.data);
+    //$scope.books = resp.data;
