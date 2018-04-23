f83189e9ddda47ab259991e7387fa973e7587cce
Rigney <krigney@proforma.com>
2018-04-23 19:07:54 -0400

    All books display upon button click.

--- LibraryDatabase/Scripts/app.js
+++ LibraryDatabase/Scripts/app.js
@@ -2,24 +2,35 @@
     module("libraryApp", [])
     .controller("mainController", ["$scope", "$http", ($scope, $http) => {
 
-        $scope.currentTime = new Date();
+        $scope.getAllBooks = () => {
+            $http({
+                method: "GET",
+                url: "/api/books"
+            }).then(resp => {
+                console.log(resp.data);
+                $scope.allBooks = resp.data;
+                })
+        }
+        
+        $scope.getAvailableBooks = () => {
+            $http({
+                method: "GET",
+                url: "/api/availablebooks"
+            }).then(resp => {
+                console.log(resp.data);
+                $scope.availableBooks = resp.data;
+            })
+        }
 
-        $http({
-            method: "GET",
-            url: "/api/books"
-        }).then(resp => {
-            console.log(resp.data);
-            $scope.books = resp.data;
-        })
+        //$scope.checkOutBook = (ID, userEmail) => {
+        //    $http({
+        //        method: "PUT",
+        //        url: 'api/CheckOutManagement/${ID}/',
+        //        data: JSON.stringify(email:'${userEmail}')
+        //    }).then(resp => {
+        //        console.log(resp.data);
+        //        $scope.checkOutReciept = resp.data;
+        //    })
+        //}
+        
     }]);
-
-//PUT WILL LOOK LIKE:
-//$http({
-//method: "PUT",//what I am doing
-  //  url: "/api/book"//where am I doing it
-    //data: JSON.stringify(email: "email from UI text box goes here.")
-
-
-      //  }).then(resp => {
-    //console.log(resp.data);
-    //$scope.books = resp.data;
